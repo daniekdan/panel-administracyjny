e1a714e04a98e3ff809ad3e674865c159b739329
sebixson <sebixson.1@gmail.com>
2021-05-27 14:51:53 +0200

    pierwsza wersja

--- index.php
+++ index.php
@@ -5,10 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="css/main.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>proto strona</title>
+  
+    <body bgcolor="purple" text="white">
+   
 </head>
 <body>
     <div class="header">
+    <header>
+    <center>
+   <h1>Dashboard</h1>
+   </center>
+    <p1>Forum</p1>
+    <br></br>
+    <p2>Uzytkownicy</p2>
+    <br></br>
+    <p3>grafika</p3>
+    <br></br>
+    <p4>Narzędzia</p4>
+    <br></br>
+    <p5>Konfiguracja</p5>
+    <br></br>
+    <p6>System</p6>
+
 
     </div>
     <div class="navbar">
@@ -18,6 +37,11 @@
         
     </div>
     <div class="footer">
+    <br></br>
+    <center>
+    <span style="font-size: small">Twórca:Sebastian Pietras</span>
+    <p>All right reservet</p>
+    </center>
 
     </div>
 </body>
