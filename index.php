d2ac382b67322b2fc9221dac7478b1faacc75380
sebixson <sebixson.1@gmail.com>
2021-05-29 20:09:01 +0200

    struktura v2

--- index.php
+++ index.php
@@ -1,48 +1,75 @@
-<!DOCTYPE html>
+<html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="css/main.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>proto strona</title>
+    <title></title>
   
-    <body bgcolor="purple" text="white">
-   
+    <body text="white">
+      <style>
+table, th, td {
+  border: 12px solid #2E85B3;
+  border-collapse: collapse;
+  
+  
+  
+}
+th, td {
+  padding: 1px;
+  text-align: center;
+  
+  
+}
+#t01 {
+  width: 90%; 
+  background-color: #87A6C4;
+}
+        #t02 {
+  width: 90%; 
+  background-color: #87A6C4;
+}
+</style>
+      
+
 </head>
 <body>
+  
     <div class="header">
     <header>
-    <center>
+        <center>
+        <table id="t01">
+          
+  <tr>
+    <th><a href="">treści</a><br><br>Tutaj mozesz dodawać, usuwać<br></r> oraz edytować  treści</br></th>
+    <th><a href="">Forum</a><br><br>Tutaj mieszczą sie informacje na<br>temat statystyk i forum</br></th> 
+    <th><a href="">Uzytkownicy</a><br><br>tutaj mozesz zarządzać<br>uzytkownikami </br></th>
+    <th><a href="">Grafika</a><br><br>w tym miejscu mozna skonfigurować<br> całą szate graficzną</br></th>
+    <th><a href="">Narzędzia</a><br><br>w tym miejscu znadziesz<br> wszystkie potrzebne narzędzia systemowe</br>
+</th>
+ <th><a href="">System</a><br><br>Informacje na temat obecnego stanu systemu<br> oraz nadchodzących aktualizacji</br>
+</th>
+  </tr>
    <h1>Dashboard</h1>
    </center>
-    <p1>Forum</p1>
-    <br></br>
-    <p2>Uzytkownicy</p2>
-    <br></br>
-    <p3>grafika</p3>
-    <br></br>
-    <p4>Narzędzia</p4>
-    <br></br>
-    <p5>Konfiguracja</p5>
-    <br></br>
-    <p6>System</p6>
+
 
 
     </div>
     <div class="navbar">
+       <tr>
+    <th><a href="">Ostatnie logi </a><br><br>Brak logowań<br></r> </br></th>
 
-    </div>
+</div>
     <div class="container">
+       <tr>
+    <th><a href="">Powiadomienia</a><br><br>Brak powiadomień<br></r> Brak powiadomień<br>Brak powiadomień<br>Brak powiadomień<br>Brak powiadomień<br></br></th>
+      
         
     </div>
     <div class="footer">
-    <br></br>
-    <center>
-    <span style="font-size: small">Twórca:Sebastian Pietras</span>
-    <p>All right reservet</p>
-    </center>
-
+      <p></p>
     </div>
 </body>
 </html>
