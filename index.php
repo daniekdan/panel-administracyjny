06ab78656bb6ebc8e0b863a4db7cc04aac0c1cb3
sebixson <84780416+sebixson@users.noreply.github.com>
2021-06-10 21:02:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
     session_start();
         if (!(isset($_SESSION['username']))) {
-             header('location: dashboard.php');
+             header('location: login.php');
             exit;
         }
     ?>
